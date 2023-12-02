207199e43884be15fc549622c120e0d7c6de2242
yolandaproensa <yolandaproensa@gmail.com>
2023-12-02 19:53:12 -0300

    Primeira execução do servidor.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Olá mundo!")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
