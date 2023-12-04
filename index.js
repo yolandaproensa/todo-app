b94862c918c6d98b2f28b1c93300bc1f72942562
yolandaproensa <yolandaproensa@gmail.com>
2023-12-04 18:21:44 -0300

    Iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -34,6 +34,24 @@ app.post('/criar', (req, res) => {
 })
 
 app.get('/', (req, res) => {
+    const sql = `
+    SELECT * FROM tarefas 
+    `
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) => {
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: dado.completa === 0 ? false : true
+            }
+        })
+    })
+
     res.render(`home`)
 })
 
