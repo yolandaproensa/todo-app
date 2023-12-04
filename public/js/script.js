ef449ac04b0b21d887f55030b67dcc0a3f50cb22
yolandaproensa <yolandaproensa@gmail.com>
2023-12-04 17:35:11 -0300

    Deixando a mudança de tema dinâmica

---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+function alterarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        let novoTema
+
+        if (tema == "light") {
+            novoTema = "dark"
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Icone de lua">`
+            body.classList.remove("light")
+            body.classList.add("dark")
+        } else {
+            novoTema = "light"
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Icone de lua">`
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+
+        localStorage.setItem("tema", novoTema)
+        return
+    }
+
+    localStorage.setItem("tema", "dark")
+    body.classList.add("dark")
+}
+
+function verificarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        if (tema == "dark") {
+            body.classList.add("dark")
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Icone de lua">`
+        } else {
+            body.classList.add("light")
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Icone de lua">`
+        }
+    }
+}
+
+verificarTema()
